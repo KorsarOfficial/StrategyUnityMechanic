6e3604a23204ea4f5d18b48b4e10e0149566de81
KorsarOfficial <bebrusponuhanus@gmail.com>
2023-05-11 14:38:07 +0400

    Unity SR
    For personal use and sale

---
+++ Unity Mechanic/Unity Mechanic/ClicknPoint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+public class ClicknPoint : MonoBehaviour
+{
+    private Camera myCam;
+
+    public LayerMask clickable;
+    public LayerMask ground;
+
+    void Start()
+    {
+        myCam = Camera.main;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, clickable))
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    UnitSelections.Instance.ShiftClickSelect(hit.collider.gameObject);
+                }
+                else
+                {
+                    UnitSelections.Instance.ClickSelect(hit.collider.gameObject);
+                }
+            }
+            else
+            {
+                if (!Input.GetKey(KeyCode.LeftShift))
+                {
+                    UnitSelections.Instance.DeselectAll();
+                }
+            }
+        }
+    }
+}
---
+++ Unity Mechanic/Unity Mechanic/Mechanic.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Mechanic : MonoBehaviour
+{
+    void Start()
+    {
+        UnitSelections.Instance.unitList.Add(this.gameObject);
+    }
+    void OnDestroy()
+    {
+        UnitSelections.Instance.unitList.Remove(this.gameObject);
+    }
+}
